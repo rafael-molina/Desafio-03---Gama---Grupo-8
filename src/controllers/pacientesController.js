c7fdd23eb5c838928671fef8481c08bdb33bcdc9
Rafael Molina <rafg.molina@gmail.com>
2022-10-17 15:21:41 -0300

    corrigindo CRUD update pacientes

--- src/controllers/pacientesController.js
+++ src/controllers/pacientesController.js
@@ -1,12 +1,12 @@
 const { Pacientes }  = require("../models");
 
-const bcrypt = require("bcryptjs"); // este modulo deve ser primeiro instalado(digite "npm i bcryptjs" no terminal), depois impotamos ele como esta feito nesta linha
+const bcrypt = require("bcryptjs"); 
 
 const pacientesController = {
     async cadastro(req, res) {
         try {
-            const { nome, email, idade } = req.body;  // aqui eu recebo todos os campos para cadastro do psicologo através do body
-            const newPaciente = await Pacientes.create({  // aqui estamos de fato criando os dados na base de dados
+            const { nome, email, idade } = req.body;  
+            const newPaciente = await Pacientes.create({  
                 nome,
                 email,
                 idade
@@ -53,7 +53,6 @@ const pacientesController = {
             const {id} = req.params;
             const {nome, email, idade} = req.body;
 
-            //if(!id) return res.status(400).json("id não enviado"); // aqui colocamos uma condição que diz: "se o id for undefined ou não for encontrado retorne o status 400, que significa bad request(não foi enviado)". Tambem é importante imprimir uma mensagem de erro através do json.
             const pacienteAtualizar = await Pacientes.update(
                 {
                  nome,
@@ -70,7 +69,7 @@ const pacientesController = {
 
             if(!dadosPaciente) return res.status(404).json("id não encontrado");
 
-            res.status(200).json("paciente atualizado com sucesso!");
+            res.status(200).json(dadosPaciente);
             
         } catch (error) {
             console.log(error)
@@ -90,10 +89,10 @@ const pacientesController = {
 
             if(!pacienteDeletar) return res.status(404).json("id não encontrado");
 
-            res.status(204).json(); // o status 204 significa "no content" mostrando que o metodo foi executado e deu certo
+            res.status(204).json(); 
             
         } catch (error) {
-            return res.status(500).json("ocorreu algum problema") // é importante ter sempre a estrutura try/catch para funções assincronas para podermos devolver alguma resposta para o nosso usuario caso ocorra um erro. É comum para o catch colocarmos o erro status(500) que significa "internal server error"
+            return res.status(500).json("ocorreu algum problema") 
             
         }
         
